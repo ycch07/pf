51a84e2e8c55e2f4a66cc73ad4207019a6059387
윤 창현 <142772606+ycch07@users.noreply.github.com>
2023-10-23 10:37:28 +0900

    Add files via upload

--- hamburger/script.js
+++ hamburger/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-    $('.toggle-menu').click (function(){
-        $(this).toggleClass('active');
+    $('.toggle-menu,#menu .main-nav a').click (function(){
+        $('.toggle-menu').toggleClass('active');
         $('#menu').toggleClass('open');
+    });
+    $("aside a.top").click(function(){
+        $("html,body").animate({scrollTop:0},1300);
     });
 });
